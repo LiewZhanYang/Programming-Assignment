3ce5c43c19c1b62f76e840c7f676c46c7e000593
LiewZhanYang <S10259432@connect.np.edu.sg>
2024-01-24 18:21:49 +0800

    Finish Option 1

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,12 +1,75 @@
 ﻿using Assignment;
+using System.Collections.Generic;
+using System.Globalization;
 
 
 // option 1
 
+List<Customer> customers = new List<Customer>();
+
+void ReadCustomerCsv(List<Customer> customers)
+{
+    using(StreamReader sr = new StreamReader("customers.csv"))
+    {
+
+        sr.ReadLine();
+
+        string line;
+        while((line = sr.ReadLine()) != null)
+        {
+            string[] data = line.Split(',');
+
+            string name = data[0];
+            int memberId = int.Parse(data[1]);
+            DateTime dob = DateTime.ParseExact(data[2], "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            string tier = data[3];
+            int points = int.Parse(data[4]);
+            int punchCard = int.Parse(data[5]);
+
+
+            PointCard rewards = new PointCard(points,punchCard)
+            {
+                Tier = tier
+
+            };
+
+            Customer customer = new Customer(name, memberId, dob)
+            {
+                Rewards = rewards,
+                OrderHistory = new List<Order>()
+
+
+
+            };
+
+            customers.Add(customer);
+
+
+
+        }
+
+    }
+
+}
+
+ReadCustomerCsv(customers);
+
+void ListAllCustomers(List<Customer> customers)
+{
+    Console.WriteLine("{0,-20}{1,-20}{2,-20}{3,-20}{4,-20}{5,-20}",
+        "Name", "Member ID", "DOB", "MemberShipStatus", "MemberShipPoints", "PunchCard"); ;
+    foreach (Customer customer in customers) 
+    {
+        Console.WriteLine("{0,-20}{1,-20}{2,-20}{3,-20}{4,-20}{5,-20}",
+            customer.Name, customer.MemberId, customer.Dob.ToString("dd/MM/yyyy")
+            ,customer.Rewards.Tier ,customer.Rewards.Points,customer.Rewards.PunchCard);
+
+    }
 
 
 
 
+}
 
 
 
@@ -89,8 +152,10 @@ while (true)
         break;
     }
     else if (option == 1) 
-    { 
-    
+    {
+        ListAllCustomers(customers);
+
+
     }
     else if (option == 2) 
     { 
