065c6bdad4bf67f1f6561f0dd7a569c7fc06cbbf
LiewZhanYang <S10259432@connect.np.edu.sg>
2024-01-24 17:04:36 +0800

    insert validation

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,1 +1,121 @@
-﻿
+﻿using Assignment;
+
+
+// option 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void displaymenu()
+{
+    Console.WriteLine("------ Menu ------");
+    Console.WriteLine("1 ) List All Customer");
+    Console.WriteLine("2 ) List All Current Order");
+    Console.WriteLine("3 ) Register A New Customer");
+    Console.WriteLine("4 ) Create A Customer's Order");
+    Console.WriteLine("5 ) Display Order Details Of A Customer");
+    Console.WriteLine("6 ) Modify Order Details");
+    Console.WriteLine("0 EXIT");
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+while (true)
+{
+    displaymenu();
+
+    Console.Write("What is your option: ");
+    int option = Convert.ToInt32(Console.ReadLine());
+
+    if (option == 0)
+    {
+        break;
+    }
+    else if (option == 1) 
+    { 
+    
+    }
+    else if (option == 2) 
+    { 
+    
+    }
+    else if (option == 3) 
+    { 
+    
+    }
+    else if (option == 4)
+    {
+
+    }
+    else if (option == 5)
+    {
+
+    }
+    else if (option == 6)
+    {
+
+    }
+
+
+
+
+
+
+}
